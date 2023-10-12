402bad4af3ba8592e02a485a768225043987fe28
abhayrsr <30930553+abhayrsr@users.noreply.github.com>
2023-10-12 16:59:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+var readlineSync = require('readline-sync');
+var username = readlineSync.question("What is your name? \n");
+console.log(`Hi ${username}! let's decide today's task`);
+
+console.log(`Please enter the tasks. Type "done" when finished adding! \n`);
+
+var tasks = [];
+while(true){
+  var subTasks = readlineSync.question();
+  if(subTasks == "done") break;
+  tasks.push(subTasks);
+}
+console.log(`here's the list of tasks: `, tasks);
+console.log("Is there any update you want to make to the list?(yes/no)");
+
+var delRequest = readlineSync.question();
+if(delRequest == "yes"){
+  console.log(`Ok, select the item number from list to delete. Type "done" when finished deleting!`)
+  while(true){
+    var delNum = readlineSync.question()
+    if(delNum == "done") break;
+    tasks.splice(delNum - 1, 1);
+  }
+} else {
+  console.log("Ok, great!");
+}
+
+console.log("Any new task you want to add?(yes/no)");
+var newRequest = readlineSync.question();
+if(newRequest == "yes"){
+  console.log(`Ok, add any new task. Type "done" when finished adding!`);
+  while(true){
+    var newTask = readlineSync.question();
+    if(newTask == "done") break;
+    tasks.push(newTask);
+  }
+} else {
+  console.log("Ok, all the best!")
+}
+
+function assume(){
+  console.log("Assume, you are done for the day. What tasks have you completed? \nType 'complete' in front of the tasks that are complete and 'incomplete' in front of the tasks that are incomplete.");
+}
+
+setTimeout(assume, 3000);
+
+var complete = [];
+var incomplete = [];
+var count = 0;
+console.log(`Write the no. of the task completed(Type 'done' when finished typing!): `);
+while(true){
+  var compTask = readlineSync.question();
+  if(compTask == "done") break;
+  if(tasks.includes(compTask)){
+    count++;
+    complete.push(compTask);
+  }
+}
+if(complete.length){
+  console.log("Task Completed: ", complete);
+} else {
+  console.log("Task Completed: none");
+}
+
+for(var i = 0; i <=  tasks.length; i++){
+  if(!complete.includes(tasks[i])){
+    incomplete.push(tasks[i]);
+  }
+}
+
+if(incomplete.length){
+  console.log("Task Incomplete: ", incomplete);
+} else {
+  console.log( "Task Incomplete: none");
+}
+
+console.log("You have completed", count, "tasks");
